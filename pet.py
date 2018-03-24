ff50fa4c8104f912564516a79b799510556507b3
Matthew Doto <mvd369@gmail.com>
2018-03-24 19:11:28 -0400

    Functions exist, API can run

---
+++ pet.py
@@ -0,0 +1,25 @@
+import connexion
+
+def get_data(petID):
+    return {'message': 'get pet data'}, 501
+
+def make_new(petID):
+    return {'message': 'make a pet'}, 501
+
+def feed(petID, foodID):
+    return {'message': 'feed pet'}, 501
+
+def play(petID, gameID):
+    return {'message': 'play with pet'}, 501
+
+def clean(petID):
+    return {'message': 'clean up poo'}, 501
+
+def wash(petID):
+    return {'message': 'wash pet'}, 501
+
+def scold(petID):
+    return {'message': 'discipline pet'}, 501
+
+def heal(petID):
+    return {'message': 'heal pet'}, 501
---
+++ user.py
@@ -0,0 +1,7 @@
+import connexion
+
+def get_data(username):
+    return {'message': 'get user data'}, 501
+
+def make_new(username):
+    return {'message': 'make a user'}, 501
